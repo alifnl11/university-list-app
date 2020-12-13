c75db26f7e0b05bd25f522e066f562af24af286d
M Alif Nurrofli L <zerpprogaming@gmail.com>
2020-12-14 00:45:38 +0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBj-_UBgYYsLXpt1ZgbApavuV0DOlp1he8",
+  apiKey: "",
   authDomain: "univ-search.firebaseapp.com",
   projectId: "univ-search",
   storageBucket: "univ-search.appspot.com",
-  messagingSenderId: "394414241932",
-  appId: "1:394414241932:web:180118f250a51bf3b323a9"
+  messagingSenderId: "",
+  appId: ""
 };
